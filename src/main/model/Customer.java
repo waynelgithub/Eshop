c4d2e9b6f84f2027527744652c418eb15b0412c8
hsuchi <hsuchi@example.com>
2022-12-15 17:32:39 +0800

    add customer form html

--- src/main/model/Customer.java
+++ src/main/model/Customer.java
@@ -6,6 +6,11 @@ import javax.validation.constraints.Size;
 
 public class Customer {
 	
+	public enum Continent {
+		台北市, 新北市, 桃園市, 新竹縣, 新竹市, 苗栗縣, 苗栗市, 台中市, 彰化縣, 彰化市, 雲林縣, 雲林市, 嘉義縣, 嘉義市, 台南市, 高雄市, 屏東縣,
+		屏東市, 台東縣, 台東市, 花蓮縣, 花蓮市, 宜蘭縣, 宜蘭市, 南投縣
+	}
+	
 	@NotBlank(message = "{cus.name}")
 	@Size(min = 1)
 	private String customerName;
