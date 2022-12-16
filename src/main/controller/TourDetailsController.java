fa4d77203f87ac04905cb3d59448dcc7e38f8f76
hsuchi <hsuchi@example.com>
2022-12-16 15:37:45 +0800

    pratice one to one part 2

--- src/main/controller/TourDetailsController.java
+++ src/main/controller/TourDetailsController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import main.model.Tour;
+import main.model.TourDetails;
 import main.service.TourDetailsService;
 import main.service.TourService;
 
@@ -25,11 +27,25 @@ public class TourDetailsController {
 		if(tour != null) {
 			tourService.addTourDetailsIfNotExists(tour);
 			model.addAttribute("tour", tour);
+			return "tour-details";
 		}
 		return "redirect:showTourDetails";
 	}
 	
-	public String editTourDetails() {
-		return "";
+	@GetMapping("/editTourDetails/{tourId}")
+	public String editTourDetails(@PathVariable long tourId, Model model) {
+		Tour tour = tourService.getById(tourId);
+		if(tour != null) {
+			tourService.addTourDetailsIfNotExists(tour);
+			model.addAttribute("tour", tour);
+			return "form-tour-details";
+		}
+		return "redirect:/showOffer";
+	}
+	
+	@GetMapping("/processForm/TourDetails/{tourId}")
+	public String processTourDetailsData(@ModelAttribute TourDetails tourDetails) {
+		tourDetailsService.saveOrUpdate(tourDetails);
+		return "redirect:/showOffer";
 	}
 }
